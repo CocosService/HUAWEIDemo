85c7f29542f5f7b06e2021fb0ed9afba88531734
wuzhiming <wuzhiming.chukong@gmail.com>
2020-09-17 18:52:08 +0800

    更新测试例

--- assets/applinking/applinking.js
+++ assets/applinking/applinking.js
@@ -15,7 +15,7 @@ cc.Class({
         window._demoAppLinking = this;
         this.console.log('please build links before share');
 
-        huawei.agc.applinking.appLinkingService.on(huawei.agc.applinking.AGC_APP_LINKING_EVENT_LISTENER_NAME.RECEIVE_LINK_CALLBACK, (data) => {
+        huawei.agc.applinking.appLinkingService.on(huawei.agc.applinking.AGC_APP_LINKING_EVENT_LISTENER_NAME.RECEIVE_LONG_LINK_CALLBACK, (data) => {
             if (data.code === 1) {
                 this.console.log('receive deepLink：', data.getDeepLink());
             }
